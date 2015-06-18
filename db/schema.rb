cc4494aa38ae0a64d665671caa781f6dc77ddabb
Mike.C <mepsmike@gmail.com>
2015-06-18 14:49:10 +0800

    add column to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618063244) do
+ActiveRecord::Schema.define(version: 20150618064629) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150618063244) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "topic_id"
   end
 
   create_table "topic_categoryships", force: :cascade do |t|
