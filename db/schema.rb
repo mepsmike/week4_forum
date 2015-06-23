99fab47662b346aa654874d7141667f4213eef53
Mike.C <mepsmike@gmail.com>
2015-06-23 17:57:19 +0800

    add_table_favorite

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622083645) do
+ActiveRecord::Schema.define(version: 20150623094749) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20150622083645) do
 
   add_index "comments", ["topic_id"], name: "index_comments_on_topic_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topic_categoryships", force: :cascade do |t|
     t.integer  "topic_id"
