2a2537573308e9105b5f2a943b9555e70ce48858
Mike.C <mepsmike@gmail.com>
2015-06-29 21:10:57 +0800

    add image and name to user

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -34,7 +34,7 @@ class TopicsController < ApplicationController
 	end
 
 	def edit
-		if @topic.user == current_user #防止使用者修改網址列修改別人的東西
+		if @topic.user == current_user 
 			render :action => :new
 		end		
 	end
@@ -51,7 +51,10 @@ class TopicsController < ApplicationController
   end
 
   def collect
+  	
+  	
   	@favorite=Favorite.create(:user_id => current_user.id,:topic_id => params[:tid]) 	
+  	
   	redirect_to topics_path
   end
 
@@ -108,7 +111,6 @@ class TopicsController < ApplicationController
 			sort_by = "view DESC"
 
 		end
-		#sort_by = (params[:order] == 'num') ? 'num DESC' : 'latesttime DESC'
 
 
 		if params[:order]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629103644) do
+ActiveRecord::Schema.define(version: 20150629115925) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 20150629103644) do
     t.string   "role",                   default: "user1"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
